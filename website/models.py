2a1fe15164eb4e0acc0edc6938f0f67945d06ab3
LucasViana18 <lucasvviana14@gmail.com>
2021-06-01 18:57:08 +0100

    Start Contact page topic to function with database

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    country = models.CharField(max_length=30)
+    subject = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
--- website/views.py
+++ website/views.py
@@ -1,17 +1,27 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from .forms import ContactForm
 
 # Create your views here.
 def home_page_view(request):
-	return render(request, 'website/index.html')
+    return render(request, 'website/index.html')
 
 def portfolio_page_view(request):
-	return render(request, 'website/portfolio.html')
+    return render(request, 'website/portfolio.html')
 
 def services_page_view(request):
-	return render(request, 'website/services.html')
+    return render(request, 'website/services.html')
 
 def aboutus_page_view(request):
-	return render(request, 'website/aboutus.html')
+    return render(request, 'website/aboutus.html')
 
 def contact_page_view(request):
-	return render(request, 'website/contact.html')
+    form = ContactForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('home'))
+
+    context = {'form': form}
+    return render(request, 'website/contact.html', context)
